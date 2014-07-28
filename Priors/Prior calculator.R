e3149a0d44663fad8d18286a127ce15daab912d8
elkelly <elkelly1210@gmail.com>
2014-07-28 14:41:14 +1000

    28/07/14

---
+++ Priors/Prior calculator.R
@@ -0,0 +1,32 @@
+setwd("/Users/ben/Documents/Papers/EvoPVA/Quoll model R/Priors")
+
+
+#litter size
+d<-read.table("Litter size.txt", header=T, sep="\t")
+
+# mean=6.725
+# sd = 1.206140
+
+#Male survival
+d<-read.table("Male survival.txt", header=T, sep="\t")
+p.ms<-d$surv/d$n
+mean(p.ms)
+sd(p.ms)
+# mean = 0.04211957
+# sd = 0.05893253
+
+#Female survival year 1
+d<-read.table("Female survival yr1.txt", header=T, sep="\t")
+p.fs1<-d$surv/d$n
+mean(p.fs1)
+sd(p.fs1)
+#mean= 0.2338186
+#sd = 0.1235535
+
+#Female survival year 2
+d<-read.table("Female survival yr2.txt", header=T, sep="\t")
+p.fs2<-d$surv/d$n
+mean(p.fs2)
+sd(p.fs2)
+#mean= 0.02020202
+#sd = 0.03499093
