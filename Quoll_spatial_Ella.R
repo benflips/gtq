cd050478ed3ffe186c2b4fc02826eb1756d24d2d
elkelly <elkelly1210@gmail.com>
2014-08-12 12:09:50 +1000

    12/08/14

--- Quoll_spatial_Ella.R
+++ Quoll_spatial_Ella.R
@@ -2,8 +2,7 @@
 
 ###Note: execution of quantitative genetics is (badly) incorrect and needs to be fixed before evolutionary scenarios can be implemented
 
-#### Ella edited
-
+#### Ella edited 12/8/14
 
 # Initialises a matrix with n individuals
 # individuals have a location (spX, spY) and are generated from
@@ -44,9 +43,10 @@ ddep1<- function(x, beta, alpha) {
 sample2<-function(v){
 	if (is.null(v)==T) return(NULL)
 	else sample(v, 1, T)	
-}r
+}
 
 #function that reproduces individuals
+#density dependance acting on fecundity according to grid cell K (beta)
 # takes population matrix
 repro<-function(popmat, spX, spY, fec, init.p.var, h, alpha, beta){
 	b.var<-h*init.p.var
@@ -63,33 +63,25 @@ repro<-function(popmat, spX, spY, fec, init.p.var, h, alpha, beta){
 	B.off<-unlist(lapply(b.list[temp], sample2)) #sample a male for each female (replace=T)
 	if (length(B.off)!=length(female[,"B"])) print("error")#browser() #error catcher!
 	B.off<-(B.off+female[,"B"])/2 #calculate midparent value	
+#matrix of females ordered by grid number
 	female<- cbind(B.off, female)
+	female.sort<-female[order(female[,"X"]),]
+	female.list<-female.sort[order(female.sort[,"Y"]),]
+#work out the no of females in each grid cell	
 	f<-table(factor(female[,"X"], levels=1:spX), factor(female[,"Y"], levels=1:spY))
+#density dependant offspring number for each grid cell
 	dd.off<- ddep1(x=f, beta, alpha)
 	off.no<-rbinom(length(f), 8, dd.off)
-	off.no<- as.integer(off.no)
-print(off.no)	
-	#off.no<- is.vector(off.no, mode="integer")
-	#off.no<-matrix(off.no, nrow=10, ncol=10)	
-	#off.no<-as.vector(off.no)
-	#Y<-rep(1:10, times=10)
-	#X<-rep(1:10, each=10)
-	#off.no<-cbind(off.no, X, Y)
-	#ind<-rep(off.no, times=f.no)
-	#all<-cbind(female, off.no)
-#print(all)
-	B<- rep(female, off.no)
-
-#print(female)
-	#B<-rep(B.off, off.no) #replicate midparent breeding values by the number of offspring each had
-#print(B)
-	
+#replicate offspring number by number of females in each grid cell	
+	no.females<-as.vector(f, mode="integer")
+	off.nos<- rep(off.no, no.females)
+#get info for next generation and combine
+	B<-rep(female[,"B.off"], off.nos)	
 	B<-rnorm(length(B), B, (b.var/2)^0.5) #mid parent breeding values with variance of surviving offspring
-#print(B)
-	X<-rep(female[,"X"], B)
-	Y<-rep(female[,"Y"], B)
-	S<-rbinom(length(X), 1, 0.5) # random sex allocation
-	A<-rep(0, length(X))	# age=0
+	X<-rep(female[,"X"], off.nos)
+	Y<-rep(female[,"Y"], off.nos)
+	S<-rbinom(length(B), 1, 0.5) # random sex allocation
+	A<-rep(0, length(B))	# age=0
 	P<-rnorm(length(B), B, (init.p.var-b.var)^0.5) #offspring phenotypes
 	off<-cbind(X, Y, S, A, B, P)
 	popmat<-rbind(popmat, off)
@@ -116,13 +108,9 @@ age<-function(popmatrix, selection=F, alpha, fsurv1, fsurv2, msurv, spX, spY, be
 	psurv[which(Ad[,"S"]==1 & Ad[,"A"]==1)]<-1 #one year old males (already gone through die off)
 	if (selection==T) psurv<-psurv*fit.func(Ad[,"P"]) #toad relative fitness
 	Ad<-subset(Ad, rbinom(length(Ad[,1]), 1, psurv)==1) # probabilistic survival
-	#Survival of juvs (density dependent)
+	#Survival of juvs only if toads are present (otherwise survival=1)
 	Juv<-subset(popmatrix, popmatrix[,"A"]==0)
-	density<-table(factor(Ad[,"X"], levels=1:spX), factor(Ad[,"Y"], levels=1:spY)) # adult density through space
-	density<-density+table(factor(Juv[,"X"], levels=1:spX), factor(Juv[,"Y"], levels=1:spY)) #plus juvenile density
-	density<-density/K # Density is measured relative to carrying capacity/habitat size
-	temp<-(Juv[,"Y"]-1)*spX+Juv[,"X"] #matrix indexes for density matrix
-	psurv<-ddep(alpha, beta, density[temp]) #juvenile survival
+	psurv<-1
 	if (selection==T) psurv<-psurv*fit.func(Juv[,"P"]) #toad relative fitness
 	Juv<-subset(Juv, rbinom(length(Juv[,1]), 1, psurv)==1) #surviving juveniles
 	# gather survivors, age them and return
@@ -156,7 +144,7 @@ neighbours.init<-function(spX, spY){
 			neigh[[(y-1)*spX+x]]<-out
 		}	
 	}
-	neigh	#return the list
+	return(neigh)	#return the list
 }
 
 # samples a matrix and returns one row of the matrix
@@ -164,13 +152,12 @@ matrix.sample<-function(mtrx, size=1){
 	mtrx[sample(seq(length(mtrx[,1])), size=size),]	
 }
 
-
 # executes nearest neighbour dispersal where individuals disperse to neighbouring cell with probability=prob.d 
 # is called after age!
 disperse<-function(popmatrix, n.list, prob.d, spX){
 	if (length(popmatrix[,1])==0) return(popmatrix)
 	disp<-rbinom(length(popmatrix[,1]), 1, prob.d) # calculate a probability of dispersal
-	disp<-which(disp==1 & popmatrix[,"A"]==1) #get indices of surviving dispersing juveniles
+	disp<-which(disp==1 & popmatrix[,"A"]==1) #get indices of surviving dispersing (MALE?) juveniles
 	sub.list<-n.list[(popmatrix[disp,"Y"]-1)*spX+popmatrix[disp,"X"]] #generate list of neighbour matrices
 	sub.list<-lapply(sub.list, matrix.sample) #samples one row from each neighbour matrix
 	sub.list<-do.call("rbind", sub.list) #places result into a matrix
@@ -178,79 +165,48 @@ disperse<-function(popmatrix, n.list, prob.d, spX){
 	popmatrix
 }
 
-plotter<-function(popmatrix, popsize, spX, spY, sel.time){
-	p.summ<-table(factor(popmatrix[,"X"], levels=1:spX), factor(popmatrix[,"Y"], levels=1:spY))
-	#p.summ<-tapply(popmatrix[,"P"], list(factor(popmatrix[,"X"], levels=1:spX), factor(popmatrix[,"Y"], levels=1:spY)), mean)
-	par(mfrow=c(2, 2))
-	image(p.summ, col=rev(heat.colors(10)), breaks=seq(0, 100, length=11), main="Population density")
-	hist(popmatrix[,"P"], xlim=c(-30, 30), main="Phenotype")
-	plot(popsize, type="l", ylab="Population Size", xlab="Generations", bty="l")
-	arrows(sel.time,0,sel.time,100000, length=0, col="red")
-	hist(popmatrix[,"B"], xlim=c(-30, 30), main="Genotype")
-}
 
-#runs model over generations
-mother<-function(n=2000, spX=10, spY=10, alpha=-1, fsurv1=0.4, fsurv2=0.1, msurv=0.1, init.b=0, init.p.var=10, h=0.3, gens=40, beta=100, K=400, fec=4, prob.d=0.1, sel.time=10, plot=FALSE){
-	pop<-init.inds(n, spX, spY, init.b, init.p.var, h)
-	n.list<-neighbours.init(spX, spY)
+
+####RUN MODEL######################################################
+
+mother<-function(n=2000, spX=10, spY=10, alpha=-1, fsurv1=0.2, fsurv2=0.02, msurv=0.04, init.b=0, init.p.var=10, h=0.3, gens=50, K=20, fec=6.7, beta=30, prob.d=0.5, sel.time=20, plot=FALSE){
+	pop<-init.inds(n, spX, spY, init.b, init.p.var, h) # create a population
+	n.list<-neighbours.init(spX, spY) # create a list of neighbours for each cell (individual?)
 	popsize<-n
 	sel<-FALSE
 	for (g in 2:gens){
-		pop<-repro(pop, spX, spY, fec, init.p.var, h)
-		pop<-mdieoff(pop, msurv)
-		if (g>sel.time) sel<-TRUE
-		pop<-age(pop, selection=sel, alpha=alpha, fsurv1=fsurv1, fsurv2=fsurv2, msurv=msurv,spX=spX, spY=spY, beta=beta, K=K)
-		if (spX!=1 & spY!=1) pop<-disperse(pop, n.list, prob.d, spX)
-		if (length(pop[,1])==0) {
+		pop<-repro(popmat=pop, spX=spX, spY=spY, fec=fec, init.p.var=init.p.var, h=h, alpha=alpha, beta=beta) # females reproduce (density dep)
+		pop<-mdieoff(pop, msurv) # males die off
+		if (g>sel.time) sel<-TRUE # have toads arrived?
+		pop<-age(pop, selection=sel, alpha=alpha, fsurv1=fsurv1, fsurv2=fsurv2, msurv=msurv,spX=spX, spY=spY, beta=beta, K=K) # everyone gets a little older
+		pop<-disperse(pop, n.list, prob.d, spX) # and the Juveniles trudge off
+		if (length(pop[,1])==0) { # did we go extinct?
 			popsize<-c(popsize, 0)
 			print(paste("The population went extinct at generation ", g, ":-("))
 			break	
 		}
-		popsize<-c(popsize, length(pop[,1]))
-		if (plot==T) plotter(pop, popsize, spX, spY, sel.time)
+		popsize<-c(popsize, length(pop[,1])) # new population size appended to popsize vector
+		if (plot==T) plotter(pop, popsize, spX, spY, sel.time, gens, fid=g)
 	}
 	list(pop, popsize)	
 }
 
-small.mother<-function(spX=1, spY=1, alpha, fsurv1, fsurv2, msurv, beta, K, fec, init.b=0, init.p.var=10, h=0.3, gens=7, prob.d=0.5, sel=FALSE, plot=FALSE){
-	#Pobassoo
-	pop.pob<-init.inds(19, spX, spY, init.b, init.p.var, h) #founders
-	pop.pob[1:11,"S"]<-0
-	pop.pob[12:19, "S"]<-1
-	popsize.pob<-11 #to collect outputs
-	sex.rat.pob<-11/8
-	K.pob<-392
-	#Astell
-	spX.as<-3 #treat astell as 1x3 space
-	pop.as<-init.inds(45, spX, spY, init.b, init.p.var, h) #founders
-	pop.as[,"X"]<-2 #start all founders at (2,1)
-	pop.as[1:34,"S"]<-0
-	pop.as[35:45, "S"]<-1
-	popsize.as<-45 #to collect outputs
-	sex.rat.as<-34/11
-	K.as<-1265/spX.as
-	n.list.as<-neighbours.init(spX=spX.as, spY)
-	for (g in 2:gens){
-		pop.pob<-repro(pop.pob, spX, spY, fec, init.p.var, h)
-		pop.pob<-mdieoff(pop.pob, msurv)
-		popsize.pob<-c(popsize.pob, length(which(pop.pob[,"S"]==0 & pop.pob[,"A"]>0)))
-		sex.rat.pob<-c(sex.rat.pob, length(which(pop.pob[,"S"]==0 & pop.pob[,"A"]>0))/length(which(pop.pob[,"S"]==1 & pop.pob[,"A"]>0)))
-		pop.pob<-age(pop.pob, sel, alpha, fsurv1, fsurv2, msurv,spX=spX, spY, beta, K.pob)
-		pop.as<-repro(pop.as, spX=spX.as, spY, fec, init.p.var, h)
-		pop.as<-mdieoff(pop.as, msurv)
-		popsize.as<-c(popsize.as, length(which(pop.as[,"S"]==0 & pop.as[,"A"]>0)))
-		sex.rat.as<-c(sex.rat.as, length(which(pop.as[,"S"]==0 & pop.as[,"A"]>0))/length(which(pop.as[,"S"]==1 & pop.as[,"A"]>0)))
-		pop.as<-age(pop.as, sel, alpha, fsurv1, fsurv2, msurv,spX=spX.as, spY, beta, K.as)
-		pop.as<-disperse(pop.as, n.list.as, prob.d, spX.as)
-		if (plot==T) plotter(pop.as, popsize.as, spX, spY, sel.time=999)
-	}	
-	keep<-4:7
-	popsize.pob<-popsize.pob[keep]
-	sex.rat.pob<-sex.rat.pob[keep]
-	popsize.as<-popsize.as[keep]
-	sex.rat.as<-sex.rat.as[keep]
-	c(popsize.pob, sex.rat.pob, popsize.as, sex.rat.as)
+
+plotter<-function(popmatrix, popsize, spX, spY, sel.time, gens, fid){
+	fname<-paste("simgen", fid, ".png", sep="")
+	#png(fname, width=12, height=12, units="cm", res=300)	
+	p.summ<-table(factor(popmatrix[,"X"], levels=1:spX), factor(popmatrix[,"Y"], levels=1:spY))
+	#p.summ<-tapply(popmatrix[,"P"], list(factor(popmatrix[,"X"], levels=1:spX), factor(popmatrix[,"Y"], levels=1:spY)), mean)
+	par(mfrow=c(2, 2))
+	image(p.summ, col=rev(heat.colors(10)), breaks=seq(0, 1000, length=11), main="Population density")
+	hist(popmatrix[,"P"], xlim=c(-30, 30), main="Phenotype")
+	plot(popsize, type="l", ylab="Population Size", xlab="Generations", bty="l", xlim=c(0, gens))
+	arrows(sel.time,0,sel.time,100000, length=0, col="red")
+	hist(popmatrix[,"B"], xlim=c(-30, 30), main="Genotype")
+	#dev.off()
 }
 
 
 
+mother(h=0.3, init.b=-5, plot=T, gens=50)
+	
